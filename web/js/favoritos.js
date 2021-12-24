8d1a8d938f10519ef785a5d448ac26ecfc314974
MaxVGC <carlos.andres12390@live.com>
2021-12-24 02:49:00 -0500

    Btn(Fav) TERMINADO2

--- web/js/favoritos.js
+++ web/js/favoritos.js
@@ -8,10 +8,8 @@ Construir();
 name();
 
 function Construir() {
-    $.get("https://api.openweathermap.org/data/2.5/weather?lat=" + position.coords.latitude + "&lon=" + position.coords.longitude + "&lang=es&units=metric&appid=" + API_KEY_OpenWeather, function (data) {
         document.getElementById('fav_link').href = "favoritos.jsp?n=" + user + "&u=" + type + "";
         document.getElementById('home_link').href = "home.jsp?n=" + user + "&u=" + type + "";
-    });
 }
 
 
