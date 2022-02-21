6241965299970f693524f17334e25bd30612881d
MaxVGC <carlos.andres12390@live.com>
2022-02-20 19:40:08 -0500

    Falta mapa del sitio

--- web/js/perfil.js
+++ web/js/perfil.js
@@ -22,7 +22,6 @@ function Construir() {
     document.getElementById('pf_link').href = "perfil.jsp?n=" + btoa(user) + "&u=" + btoa(type) + "&i=" + btoa(img);
     document.getElementById('username').innerHTML = user;
     document.getElementById('role').innerHTML = type;
-    document.getElementById('img_perfil').src = img;
     if (params.get('i') !== null) {
         let aux = document.getElementsByClassName('img_img');
         document.getElementById('img_perfil').src = img;
